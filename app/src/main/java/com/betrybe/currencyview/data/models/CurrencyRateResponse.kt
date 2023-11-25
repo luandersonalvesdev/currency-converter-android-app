7d2d48bcdf5656c89d49a38a45b8935d23b9d53f
Luanderson Alves <luaoderson@gmail.com>
2023-11-25 18:24:09 -0300

    feat: data classes for the API response

---
+++ app/src/main/java/com/betrybe/currencyview/data/models/CurrencyRateResponse.kt
@@ -0,0 +1,8 @@
+package com.betrybe.currencyview.data.models
+
+data class CurrencyRateResponse (
+    val success: Boolean,
+    val base: String,
+    val data: String,
+    val rates: Map<String, Double>
+)
